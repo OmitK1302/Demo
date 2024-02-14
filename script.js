6766fb431c98f22c599f1aebe7a1b33b41d97cd3
Omit.Kr <omitkumar1854@gmail.com>
2024-02-14 20:16:42 +0530

    Something

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.querySelector('button').addEventListener('click', function () {
-    document.querySelector('Button').textContent = 'Clicked';
+    document.querySelector('p').style.color = '#ae51fa';
 });
